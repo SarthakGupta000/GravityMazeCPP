82b1b23f6dcb15ce08ccb52dbad67916af1dc4d6
Sarthak Gupta <sarthaksrws@icloud.com>
2026-05-26 15:55:24 +0530

    Completing full utility design

--- util.cpp
+++ util.cpp
@@ -50,7 +50,7 @@ int checkIfInvalid(int x, int y, int height, int width) {
 // prioritizes player movement over gravity
 Vec2 handleMovement(Vec2 playerInput, char **grid, int height, int width) {
     Vec2 gravity(0, 1);
-    int playerY;
+    int playerY; // if everything works well, player will be there
     int playerX;
     for (int j = 0; j < height; j++) {
         for (int i = 0; i < width; i++) {
@@ -64,11 +64,11 @@ Vec2 handleMovement(Vec2 playerInput, char **grid, int height, int width) {
     playerPos.x += playerInput.x;
     playerPos.y += playerInput.y;
     int moveValidity = checkIfInvalid(playerPos.x, playerPos.y, height, width);
-    if (moveValidity == 1) {
+    if (moveValidity == 1) { // discard if player moved wrong
         std::cout << "Invalid move, turn has been skipped" << std::endl;
         playerPos.x = playerX;
         playerPos.y = playerY;
-        return playerPos;
+        return Vec2(playerPos.x - playerX, playerPos.y - playerY);
     }
     moveValidity = checkIfInvalid(playerPos.x + gravity.x, playerPos.y + gravity.y, height, width);
     if (moveValidity == 1) {
@@ -76,29 +76,29 @@ Vec2 handleMovement(Vec2 playerInput, char **grid, int height, int width) {
             std::cout << "Invalid move, turn has been skipped" << std::endl;
             playerPos.x = playerX;
             playerPos.y = playerY;
-            return playerPos;
+            return Vec2(playerPos.x - playerX, playerPos.y - playerY);
         }
-        if (grid[playerPos.y][playerPos.x] == '.' || grid[playerPos.y][playerPos.x] == 'E') {
-            return playerPos;
+        if (grid[playerPos.y][playerPos.x] == '.' || grid[playerPos.y][playerPos.x] == 'E') { // keep player only if gravity moved wrong
+            return Vec2(playerPos.x - playerX, playerPos.y - playerY);
         }
     }
-    if (grid[playerPos.y][playerPos.x] == '#' && grid[playerPos.y + gravity.y][playerPos.x + gravity.x] == '#') {
+    if (grid[playerPos.y][playerPos.x] == '#' && grid[playerPos.y + gravity.y][playerPos.x + gravity.x] == '#') { // discard if player and gravity both move into a wall
         std::cout << "Invalid move, turn has been skipped" << std::endl;
         playerPos.x = playerX;
         playerPos.y = playerY;
-        return playerPos;
+        return Vec2(playerPos.x - playerX, playerPos.y - playerY);
     }
     if ((grid[playerPos.y][playerPos.x] == '.' || grid[playerPos.y][playerPos.x] == 'E') && grid[playerPos.y + gravity.y][playerPos.x + gravity.x] == '#') {
-        return playerPos;
+        return Vec2(playerPos.x - playerX, playerPos.y - playerY);
     }
     if (grid[playerPos.y][playerPos.x] == '#' && (grid[playerPos.y + gravity.y][playerPos.x + gravity.x] == '.' || grid[playerPos.y + gravity.y][playerPos.x + gravity.x] == 'E')) {
         playerPos.x += gravity.x;
         playerPos.y += gravity.y;
-        return playerPos;
+        return Vec2(playerPos.x - playerX, playerPos.y - playerY);
     }
     playerPos.x += gravity.x;
     playerPos.y += gravity.y;
-    return playerPos;
+    return Vec2(playerPos.x - playerX, playerPos.y - playerY);
 }
 
 void runGame(char **grid, int height, int width) {
@@ -144,9 +144,9 @@ int main() { // for testing
     // . # # # # # # # # # 
     // . . . . . . . . . . 
     // . . . . . . . . . . 
-    // . E . . . . . . . . 
     // . . . . . . . . . . 
     // . . . . . . . . . . 
+    // . . . . . . . . . E 
     for (int i = 0; i < 10; i++) {
         grid[1][i] = '#';
         grid[4][i] = '#';
